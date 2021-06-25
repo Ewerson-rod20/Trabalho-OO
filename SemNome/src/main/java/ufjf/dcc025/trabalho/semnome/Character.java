bb8ffa3f6226be1b86de0abbcef7be2909c950d1
Daniel Muller <danielmrezende@yahoo.com.br>
2021-06-25 17:22:40 -0300

    Funcoes basicas classe Character

--- SemNome/src/main/java/ufjf/dcc025/trabalho/semnome/Character.java
+++ SemNome/src/main/java/ufjf/dcc025/trabalho/semnome/Character.java
@@ -1,14 +1,46 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ufjf.dcc025.trabalho.semnome;
 
-/**
- *
- * @author danie
- */
 public class Character {
     
+    private int classe; // Classe do guerreiro;
+    private int atkR;   // Ataque rápido;
+    private int atkL;   // Ataque lento;
+    private int def;    // Defesa;
+    
+    public Character(){
+        
+    }
+    
+    protected int getClasse(){
+        return this.classe;
+    }
+    
+    protected int getAtkR(){
+        return this.atkR;
+    }
+    
+    protected int getAtkL(){
+        return this.atkL;
+    }
+    
+    protected int getDef(){
+        return this.def;
+    }
+    
+    protected void setClasse(int classe){
+        this.classe = classe;
+    }
+    
+    protected void setAtkR(int atkR){
+        this.atkR = atkR;
+    }
+    
+    protected void setAtkL(int atkL){
+        this.atkL = atkL;
+    }
+    
+    protected void setDef(int def){
+        this.def = def;
+    }
 }
