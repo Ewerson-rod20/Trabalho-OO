8c5ad422296ecad804d3f794918408a78a697dfb
Daniel Muller <danielmrezende@yahoo.com.br>
2021-07-09 18:30:17 -0300

    Alterando nome da class principal.

--- SemNome/src/main/java/ufjf/dcc025/trabalho/semnome/Main.java
+++ SemNome/src/main/java/ufjf/dcc025/trabalho/semnome/Main.java
@@ -5,7 +5,7 @@ package ufjf.dcc025.trabalho.semnome;
  * Daniel Muller Rezende - 202065020A
  */
 
-public class Master {
+public class Main {
     public static void main(String[] args){
         
     }
