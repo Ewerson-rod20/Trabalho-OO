64f2118efa6236a9e2b1b43dc82663930d185ca1
Daniel Muller <danielmrezende@yahoo.com.br>
2021-07-11 10:13:20 -0300

    Removendo comentarios

--- SemNome/src/main/java/ufjf/dcc025/trabalho/semnome/Login.java
+++ SemNome/src/main/java/ufjf/dcc025/trabalho/semnome/Login.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ufjf.dcc025.trabalho.semnome;
 
-/**
- *
- * @author ewe88
- */
 public class Login {
     
     public void ListaDeJogadores () {     // Exibe lista de jogadores.
