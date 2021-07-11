e320e4b622bed99c5bce302efbbf3e912e368788
Ewerson-rod20 <85942045+Ewerson-rod20@users.noreply.github.com>
2021-07-11 01:11:16 -0300

    Add files via upload
    Adiciona estrutura da classe login

---
+++ SemNome/src/main/java/ufjf/dcc025/trabalho/semnome/Login.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ufjf.dcc025.trabalho.semnome;
+
+/**
+ *
+ * @author ewe88
+ */
+public class Login {
+    
+    public void ListaDeJogadores () {     // Exibe lista de jogadores.
+        
+    }
+
+    protected String CriaJogador (){        // Cria novo jogador.
+        return null;
+    }
+    
+    protected String ExcluiJogador () {     // Exclui jogador existente.
+        return null;
+    }
+}   
