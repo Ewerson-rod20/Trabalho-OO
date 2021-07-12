142f6381114806c8a8abbdeb69d24da72a51f5aa
Daniel Muller <danielmrezende@yahoo.com.br>
2021-07-12 13:29:02 -0300

    Modificando nome

---
+++ FightingDuel/src/main/java/ufjf/dcc025/trabalho/fightingduel/Login.java
@@ -0,0 +1,19 @@
+
+package ufjf.dcc025.trabalho.fightingduel;
+/**
+ * @author Ewerson dos Santos Rodrigues (201965029AB)
+ */
+public class Login {
+    
+    public void ListaDeJogadores () {     // Exibe lista de jogadores.
+        
+    }
+
+    protected String CriaJogador (){        // Cria novo jogador.
+        return null;
+    }
+    
+    protected String ExcluiJogador () {     // Exclui jogador existente.
+        return null;
+    }
+}   
---
+++ FightingDuel/src/main/java/ufjf/dcc025/trabalho/fightingduel/Main.java
@@ -0,0 +1,15 @@
+
+package ufjf.dcc025.trabalho.fightingduel;
+
+/**
+ * Daniel Muller Rezende - 202065020A
+ * Ewerson dos Santos Rodrigues - 201965029AB
+ * Pedro Luiz Bonorino Braga - 202065177A
+ * Vitor Fernandes Reis - 202065096A
+ */
+
+public class Main {
+    public static void main(String[] args){
+        
+    }
+}
