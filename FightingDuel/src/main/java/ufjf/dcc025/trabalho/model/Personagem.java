95c71219955ab2831e34a1d7ceb035153870f27d
Daniel Muller <danielmrezende@yahoo.com.br>
2021-08-10 14:39:08 -0300

    FightingDuel
    Desenvolvendo personagem

--- FightingDuel/src/main/java/ufjf/dcc025/trabalho/model/Personagem.java
+++ FightingDuel/src/main/java/ufjf/dcc025/trabalho/model/Personagem.java
@@ -1,58 +1,28 @@
 
 package ufjf.dcc025.trabalho.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Daniel Muller Rezende (202065020A)
  */
 public class Personagem {
     
-    private String nome;         // Nome do personagem;
-    private int classe;          // Classe do personagem;
-    private int vida;            // Vida do personagem;
-    private int atkR;            // Ataque rápido/fraco;
-    private int atkL;            // Ataque lento/forte;
-    private int def;             // Defesa;
-    private int conhecimento;    // Pontos de conhecimento;
-    private int pontosAtributos; // Pontos para aumentar os atributos (vida, atk, def e conhecimento) do personagem;
+    private String nome;            // Nome do personagem;
+    private int classe;             // Classe do personagem;
+    private int vida;               // Vida do personagem;
+    private int nivel;              // Nível do personagem;
     
-//-----------------------------------------------------------------------------------------------------------------
-// Constructor;
+    
+// Construtor ------------------------------------------------------------------
     
     public Personagem(int classe){
         this.classe = classe;
+        
+        // Define o conjunto de ataques de acordo com sua classe;
+        
     }
     
-//-----------------------------------------------------------------------------------------------------------------
-// Métodos geteres;
-    
-    protected String getNome(){
-        return this.nome;
-    }
-    
-    protected int getClasse(){
-        return this.classe;
-    }
-    
-    protected int getVida(){
-        return this.vida;
-    }
-    
-    protected int getAtkR(){
-        return this.atkR;
-    }
-    
-    protected int getAtkL(){
-        return this.atkL;
-    }
-    
-    protected int getDef(){
-        return this.def;
-    }
-    
-    protected int getConhecimento(){
-        return this.conhecimento;
-    }
-    
-     protected int getPntsAtributos(){
-        return this.pontosAtributos;
-    }
+// Métodos geteres -------------------------------------------------------------
 }
