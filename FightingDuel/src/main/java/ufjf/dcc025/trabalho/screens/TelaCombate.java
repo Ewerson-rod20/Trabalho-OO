b47b57033cd27ae4109827701b9bba55a3d7d120
Daniel Muller <danielmrezende@yahoo.com.br>
2021-08-11 15:57:48 -0300

    Criando pacote para as classes das telas

--- FightingDuel/src/main/java/ufjf/dcc025/trabalho/screens/TelaCombate.java
+++ FightingDuel/src/main/java/ufjf/dcc025/trabalho/screens/TelaCombate.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ufjf.dcc025.trabalho.view;
+package ufjf.dcc025.trabalho.screens;
 
 /**
  *
  * @author danie
  */
-public class CombateGUI {
+public class TelaCombate {
     
 }
