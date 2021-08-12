364ea45397ce8cbb0303c9faf382aa8c3b16ff16
Daniel Muller <danielmrezende@yahoo.com.br>
2021-08-12 15:53:45 -0300

    Desenvolvendo personagem

--- FightingDuel/src/main/java/ufjf/dcc025/trabalho/character/ClassePersonagem.java
+++ FightingDuel/src/main/java/ufjf/dcc025/trabalho/character/ClassePersonagem.java
@@ -12,8 +12,6 @@ package ufjf.dcc025.trabalho.character;
 public class ClassePersonagem {
     
     // Atributos ---------------------------------------------------------------
-     private int vida;
-     
     // Atributos do ataque 1:
     private String nome1;
     private int ataque1;
