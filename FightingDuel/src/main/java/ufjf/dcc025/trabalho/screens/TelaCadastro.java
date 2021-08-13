d172c6a38e711c48c4d0e3353a79a3ad6f0a1e76
Daniel Muller <danielmrezende@yahoo.com.br>
2021-08-13 11:41:14 -0300

    Desenvolvendo telas principal de de cadastro

--- FightingDuel/src/main/java/ufjf/dcc025/trabalho/screens/TelaCadastro.java
+++ FightingDuel/src/main/java/ufjf/dcc025/trabalho/screens/TelaCadastro.java
@@ -5,10 +5,66 @@
  */
 package ufjf.dcc025.trabalho.screens;
 
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 /**
  *
  * @author danie
  */
 public class TelaCadastro {
     
+    // Construtor --------------------------------------------------------------
+    public TelaCadastro(){
+    }
+    
+    // Desenha -----------------------------------------------------------------
+    public JPanel desenha(){
+        JPanel painel = new JPanel();
+        
+        JLabel jlID = new JLabel("Identificação dos desenvolvedores.");
+        
+        painel.setLayout(new GridLayout(0, 1));
+        
+        painel.add(jlID);
+        
+        return painel;
+    }
+    
+    // Desenha Botões ----------------------------------------------------------
+    public JPanel desenhaBotoes(){
+        
+        JPanel painel = new JPanel();
+        
+        JButton botaoJogador = new JButton("Jogador");
+        JButton botaoAdministrador = new JButton("Administrador");
+        JButton botaoOrganizador = new JButton("Organizador");
+        
+        painel.add(botaoJogador);
+        painel.add(botaoAdministrador);
+        painel.add(botaoOrganizador);
+        
+        return painel;
+    }
+    
+    // Main --------------------------------------------------------------------
+    public static void main(String[] args){
+        
+        JFrame tela = new JFrame("Seleção de Cadastro");
+        TelaCadastro telaCadastro = new TelaCadastro();
+        
+        tela.setSize(1270, 720);
+        
+        tela.setLayout(new BorderLayout());
+        
+        tela.add(telaCadastro.desenhaBotoes(), BorderLayout.CENTER);
+        tela.add(telaCadastro.desenha(), BorderLayout.SOUTH);
+        
+        tela.setVisible(true);
+        tela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 }
--- FightingDuel/src/main/java/ufjf/dcc025/trabalho/screens/TelaPrincipal.java
+++ FightingDuel/src/main/java/ufjf/dcc025/trabalho/screens/TelaPrincipal.java
@@ -5,10 +5,68 @@
  */
 package ufjf.dcc025.trabalho.screens;
 
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.HeadlessException;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.plaf.basic.BasicOptionPaneUI;
+
 /**
  *
  * @author danie
  */
 public class TelaPrincipal {
+
+    // Construtor --------------------------------------------------------------
+    public TelaPrincipal(){ 
+    }
     
+    // Desenha -----------------------------------------------------------------
+    public JPanel desenha(){
+        
+        JPanel painel = new JPanel();
+        
+        JLabel jlID = new JLabel("Identificação dos desenvolvedores.");
+        
+        painel.setLayout(new GridLayout(0, 1));
+        
+        painel.add(jlID);
+        
+        return painel;
+    }
+    
+    // Desenha Botões ----------------------------------------------------------
+    public JPanel desenhaBotoes(){
+        
+        JPanel painel = new JPanel();
+        
+        JButton botaoCadastrar = new JButton("Cadastrar");
+        JButton botaoLogar = new JButton("Login");
+        
+        painel.add(botaoLogar);
+        painel.add(botaoCadastrar);
+        
+        return painel;
+    }
+    
+    // Main --------------------------------------------------------------------
+    public static void main(String[] args){
+        
+        JFrame tela = new JFrame("Tela Principal");
+        TelaPrincipal telaPrincipal = new TelaPrincipal();
+        
+        tela.setSize(1270, 720);
+        
+        tela.setLayout(new BorderLayout());
+        
+        tela.add(telaPrincipal.desenhaBotoes(), BorderLayout.CENTER);
+        tela.add(telaPrincipal.desenha(), BorderLayout.SOUTH);
+        
+        tela.setVisible(true);
+        tela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 }
