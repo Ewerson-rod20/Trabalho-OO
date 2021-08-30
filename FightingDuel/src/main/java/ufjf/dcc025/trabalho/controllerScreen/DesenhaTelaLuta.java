bf191ed9eb73a624ce9965467ffddbc86fb0d426
Vitor Fernandes <vitor.fernandes@estudante.ufjf.br>
2021-08-30 17:44:44 -0300

    Descartando imports desnecessários em DesenhaTelaLuta

--- FightingDuel/src/main/java/ufjf/dcc025/trabalho/controllerScreen/DesenhaTelaLuta.java
+++ FightingDuel/src/main/java/ufjf/dcc025/trabalho/controllerScreen/DesenhaTelaLuta.java
@@ -1,26 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package ufjf.dcc025.trabalho.controllerScreen;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.JFrame;
 import ufjf.dcc025.trabalho.modelCharacter.Oponente;
 import ufjf.dcc025.trabalho.modelCharacter.Personagem;
 import ufjf.dcc025.trabalho.viewScreens.TelaLuta;
-import ufjf.dcc025.trabalho.viewUsers.OponenteGUI;
-import ufjf.dcc025.trabalho.viewUsers.PersonagemGUI;
-        
 
-
+       
 /**
- *
  * @author vitor
  */
+
 public class DesenhaTelaLuta implements ActionListener{
     
     Personagem personagem;
@@ -32,12 +22,10 @@ public class DesenhaTelaLuta implements ActionListener{
         this.oponente = oponente;
         tela = new TelaLuta();
     }
-    
-    
-    
+        
     @Override
     public void actionPerformed(ActionEvent ae) {
         tela.chama(this.personagem, this.oponente);
-    }
-    
+    }   
 }
+
